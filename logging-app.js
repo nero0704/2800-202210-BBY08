81e52d953fd860c3e85d0e1811ed3d70371ad2a5
David Nam <knam13@my.bcit.ca>
2022-05-05 02:56:49 -0700

    Changed Branch Name from David_Web_Template to David_Login_Page. Designed Login Page, with custom fonts and Remember Me function.

--- logging-app.js
+++ logging-app.js
@@ -8,177 +8,176 @@ const { JSDOM } = require("jsdom");
 // static path mappings
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
-app.use("/img", express.static("public/imgs"));
-app.use("/fonts", express.static("public/fonts"));
+app.use("/img", express.static("public/img"));
+app.use("/fonts", express.static("public/font"));
 app.use("/html", express.static("public/html"));
 app.use("/media", express.static("public/media"));
 
 app.use(
-    session({
-        secret: "extra text that no one will guess",
-        name: "wazaSessionID",
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: "extra text that no one will guess",
+    name: "wazaSessionID",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
-app.get("/", function (req, res) {
-    if (req.session.loggedIn) {
-        res.redirect("/main");
-    } else {
-        let doc = fs.readFileSync("./public/html/index.html", "utf8");
+app.get("/", function(req, res) {
+  if (req.session.loggedIn) {
+    res.redirect("/main");
+  } else {
+    let doc = fs.readFileSync("./public/html/login.html", "utf8");
 
-        res.set("Server", "Wazubi Engine");
-        res.set("X-Powered-By", "Wazubi");
-        res.send(doc);
-    }
+    res.set("Server", "Wazubi Engine");
+    res.set("X-Powered-By", "Wazubi");
+    res.send(doc);
+  }
 });
 
-app.get("/signup", function (req, res) {
-    {
-        let signup = fs.readFileSync("./public/html/signup.html", "utf8");
-        let signupDOM = new JSDOM(signup)
-        res.set("Server", "Wazubi Engine");
-        res.set("X-Powered-By", "Wazubi");
-        res.send(signupDOM.serialize());
-    }
+app.get("/signup", function(req, res) {
+  {
+    let signup = fs.readFileSync("./public/html/signup.html", "utf8");
+    let signupDOM = new JSDOM(signup)
+    res.set("Server", "Wazubi Engine");
+    res.set("X-Powered-By", "Wazubi");
+    res.send(signupDOM.serialize());
+  }
 });
 
-app.get("/main", function (req, res) {
-    // check for a session first!
-    if (req.session.loggedIn) {
-        let main = fs.readFileSync("./public/html/main.html", "utf8");
-        let mainDOM = new JSDOM(main);
-        res.set("Server", "Wazubi Engine");
-        res.set("X-Powered-By", "Wazubi");
-        res.send(mainDOM.serialize());
-    } else {
-        // not logged in - no session and no access, redirect to home!
-        res.redirect("/");
-    }
+app.get("/main", function(req, res) {
+  // check for a session first!
+  if (req.session.loggedIn) {
+    let main = fs.readFileSync("./public/html/main.html", "utf8");
+    let mainDOM = new JSDOM(main);
+    res.set("Server", "Wazubi Engine");
+    res.set("X-Powered-By", "Wazubi");
+    res.send(mainDOM.serialize());
+  } else {
+    // not logged in - no session and no access, redirect to home!
+    res.redirect("/");
+  }
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Notice that this is a "POST"
-app.post("/login", function (req, res) {
-    res.setHeader("Content-Type", "application/json");
+app.post("/login", function(req, res) {
+  res.setHeader("Content-Type", "application/json");
 
-    console.log("What was sent", req.body.email, req.body.password);
+  console.log("What was sent", req.body.email, req.body.password);
 
-    let results = authenticate(
-        req.body.email,
-        req.body.password,
-        function (userRecord) {
-            //console.log(rows);
-            if (userRecord == null) {
-                // server couldn't find that, so use AJAX response and inform
-                // the user. when we get success, we will do a complete page
-                // change. Ask why we would do this in lecture/lab :)
-                res.send({ status: "fail", msg: "User account not found." });
-            } else {
-                // authenticate the user, create a session
-                req.session.loggedIn = true;
-                req.session.email = userRecord.email;
-                req.session.firstName = userRecord.firstName;
-                req.session.lastName = userRecord.lastName;
-                req.session.userName = userRecord.userName;
-                req.session.gender = userRecord.gender;
-                req.session.age = userRecord.age;
-                req.session.role = userRecord.role;
-                req.session.save(function (err) { });
-                res.send({ status: "success", msg: "Logged in." });
-            }
-        }
-    );
+  let results = authenticate(
+    req.body.email,
+    req.body.password,
+    function(userRecord) {
+      //console.log(rows);
+      if (userRecord == null) {
+        // server couldn't find that, so use AJAX response and inform
+        // the user. when we get success, we will do a complete page
+        // change. Ask why we would do this in lecture/lab :)
+        res.send({ status: "fail", msg: "User account not found." });
+      } else {
+        // authenticate the user, create a session
+        req.session.loggedIn = true;
+        req.session.email = userRecord.email;
+        req.session.firstName = userRecord.firstName;
+        req.session.lastName = userRecord.lastName;
+        req.session.userName = userRecord.userName;
+        req.session.gender = userRecord.gender;
+        req.session.age = userRecord.age;
+        req.session.role = userRecord.role;
+        req.session.save(function(err) {});
+        res.send({ status: "success", msg: "Logged in." });
+      }
+    }
+  );
 });
 
-app.post('/signup', function (req, res) {
+app.post('/signup', function(req, res) {
 
-    var mysql = require("mysql2");
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "COMP2800",
-    });
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var email = req.body.email;
-    var password = req.body.password;
-    var username = req.body.username;
-    var mbti = req.body.mbti;
-    var age = req.body.age;
+  var mysql = require("mysql2");
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "COMP2800",
+  });
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+  var password = req.body.password;
+  var username = req.body.username;
+  var mbti = req.body.mbti;
+  var age = req.body.age;
 
-    connection.connect(function (err) {
-        if (err) throw err;
-        var sql = "SELECT * FROM BBY_8user WHERE email =?";
-        connection.query(sql, email, function (err, data, fields){
-            if(err) throw err;
-            if(data.length > 1){
-                res.setHeader("Content-Type", "application/json");
-                res.send({status: "fail" , msg: "Email already exists."});
-            } else {
-                var sql = "INSERT INTO BBY_8user (firstName, lastname, email, password, role, userName, age, personality) VALUES ('" + fname + "', '" + lname + "', '" + email + "', '" + password + "', 'R', '" + username + "', '" + age + "', '" + mbti + "')"
-                connection.query(sql, function (err, result) {
-                    if (err) throw err;
-                    console.log("1 record inserted");
-                    res.setHeader("Content-Type", "application/json");
-                    res.send({status: "success"});
-                });
-            }
-        })
-    });
+  connection.connect(function(err) {
+    if (err) throw err;
+    var sql = "SELECT * FROM BBY_8user WHERE email =?";
+    connection.query(sql, email, function(err, data, fields) {
+      if (err) throw err;
+      if (data.length > 1) {
+        res.setHeader("Content-Type", "application/json");
+        res.send({ status: "fail", msg: "Email already exists." });
+      } else {
+        var sql = "INSERT INTO BBY_8user (firstName, lastname, email, password, role, userName, age, personality) VALUES ('" + fname + "', '" + lname + "', '" + email + "', '" + password + "', 'R', '" + username + "', '" + age + "', '" + mbti + "')"
+        connection.query(sql, function(err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+          res.setHeader("Content-Type", "application/json");
+          res.send({ status: "success" });
+        });
+      }
+    })
+  });
 })
 
-app.get("/logout", function (req, res) {
-    if (req.session) {
-        req.session.destroy(function (error) {
-            if (error) {
-                res.status(400).send("Unable to log out");
-            } else {
-                res.redirect("/");
-            }
-        });
-    }
+app.get("/logout", function(req, res) {
+  if (req.session) {
+    req.session.destroy(function(error) {
+      if (error) {
+        res.status(400).send("Unable to log out");
+      } else {
+        res.redirect("/");
+      }
+    });
+  }
 });
 
 function authenticate(email, pwd, callback) {
-    const mysql = require("mysql2");
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "COMP2800",
-    });
-    connection.connect();
-    connection.query(
-        "SELECT * FROM BBY_8user WHERE email = ? AND password = ?",
-        [email, pwd],
-        function (error, results, fields) {
-            // results is an array of records, in JSON format
-            // fields contains extra meta data about results
-            console.log(
-                "Results from DB",
-                results,
-                "and the # of records returned",
-                results.length
-            );
+  const mysql = require("mysql2");
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "COMP2800",
+  });
+  connection.connect();
+  connection.query(
+    "SELECT * FROM BBY_8user WHERE email = ? AND password = ?", [email, pwd],
+    function(error, results, fields) {
+      // results is an array of records, in JSON format
+      // fields contains extra meta data about results
+      console.log(
+        "Results from DB",
+        results,
+        "and the # of records returned",
+        results.length
+      );
 
-            if (error) {
-                // in production, you'd really want to send an email to admin but for now, just console
-                console.log(error);
-            }
-            if (results.length > 0) {
-                // email and password found
-                return callback(results[0]);
-            } else {
-                // user not found
-                return callback(null);
-            }
-        }
-    );
+      if (error) {
+        // in production, you'd really want to send an email to admin but for now, just console
+        console.log(error);
+      }
+      if (results.length > 0) {
+        // email and password found
+        return callback(results[0]);
+      } else {
+        // user not found
+        return callback(null);
+      }
+    }
+  );
 }
 
 // /*
---
+++ public/js/login.js
@@ -0,0 +1,122 @@
+ready(function() {
+
+  console.log("Client script loaded.");
+
+  function ajaxGET(url, callback) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        //console.log('responseText:' + xhr.responseText);
+        callback(this.responseText);
+
+      } else {
+        console.log(this.status);
+      }
+    }
+    xhr.open("GET", url);
+    xhr.send();
+  }
+
+  function ajaxPOST(url, callback, data) {
+
+    let params = typeof data == 'string' ? data : Object.keys(data).map(
+      function(k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+    ).join('&');
+    console.log("params in ajaxPOST", params);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        //console.log('responseText:' + xhr.responseText);
+        callback(this.responseText);
+
+      } else {
+        console.log(this.status);
+      }
+    }
+    xhr.open("POST", url);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+  }
+
+  function getSavedLogin() {
+    return new Promise((resolve, reject) => {
+      if (localStorage.getItem("AudioCave_Email") != null &&
+        localStorage.getItem("AudioCave_Password") != null) {
+        let email = localStorage.getItem("AudioCave_Email");
+        let password = localStorage.getItem("AudioCave_Password");
+        let credential = {
+          email: email,
+          password: password
+        };
+        resolve(credential);
+      } else {
+        reject(new Error("No Saved Login."));
+      }
+    })
+  }
+
+  getSavedLogin()
+    .then((cred) => {
+      let queryString = "email=" + cred.email + "&password=" + cred.password;
+      console.log(queryString);
+      ajaxPOST("/login", function(data) {
+        if (data) {
+          let dataParsed = JSON.parse(data);
+          console.log(dataParsed);
+          if (dataParsed.status == "fail") {
+            document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+          } else {
+            window.location.replace("/main");
+          }
+        }
+
+      }, queryString);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+
+  document.querySelector("#register").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.location.replace("/signup");
+  })
+
+  // POST TO THE SERVER
+  document.querySelector("#submit").addEventListener("click", function(e) {
+    e.preventDefault();
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let saveLogin = document.getElementById("save-login");
+    let queryString = "email=" + email.value + "&password=" + password.value;
+    if (saveLogin) {
+      localStorage.setItem("AudioCave_Email", email.value);
+      localStorage.setItem("AudioCave_Password", password.value);
+    }
+    ajaxPOST("/login", function(data) {
+
+      if (data) {
+        let dataParsed = JSON.parse(data);
+        console.log(dataParsed);
+        if (dataParsed.status == "fail") {
+          document.getElementById("errorMsg").innerHTML = dataParsed.msg;
+        } else {
+          window.location.replace("/main");
+        }
+      }
+
+    }, queryString);
+  });
+});
+
+function ready(callback) {
+  if (document.readyState != "loading") {
+    callback();
+    console.log("ready state is 'complete'");
+  } else {
+    document.addEventListener("DOMContentLoaded", callback);
+    console.log("Listener was invoked");
+  }
+}
