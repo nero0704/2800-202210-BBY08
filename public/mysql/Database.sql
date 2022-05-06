28b760ae61f58d1a6a00f3afa6b1cae065367327
nathan <fung.nathan1@gmail.com>
2022-05-06 12:34:19 -0700

    Added preliminary tables for survey, music and library

--- public/mysql/Database.sql
+++ public/mysql/Database.sql
@@ -1,16 +1,37 @@
 CREATE DATABASE IF NOT EXISTS COMP2800;
         use COMP2800;
         CREATE TABLE IF NOT EXISTS BBY_8user (
-        ID int NOT NULL AUTO_INCREMENT,
-        firstName varchar(30),
-        lastName varchar(30),
-        email varchar(30),
-        password varchar(30),
-        role char,
-        userName varchar(30),
-        age int,
-        personality varchar(30),
-        PRIMARY KEY (ID));
+            ID int NOT NULL AUTO_INCREMENT,
+            firstName varchar(30),
+            lastName varchar(30),
+            email varchar(30),
+            password varchar(30),
+            role char,
+            userName varchar(30),
+            age int,
+            personality varchar(30),
+            PRIMARY KEY (ID));
+        use COMP2800;
+        CREATE TABLE IF NOT EXISTS BBY_8survey (
+            userID int NOT NULL,
+            dateOfSurvey DATE,
+            survey varchar(30),
+            PRIMARY KEY (userID, dateOfSurvey),
+            FOREIGN KEY (userID) REFERENCES BBY_8user(ID)
+        );
+        CREATE TABLE IF NOT EXISTS BBY_8music (
+            ID int NOT NULL AUTO_INCREMENT,
+            songName varchar(30),
+            songArtists varchar(30),
+            songPersonality varchar(30),
+            PRIMARY KEY (ID)
+        );
+        CREATE TABLE IF NOT EXISTS BBY_8library (
+            userID int NOT NULL,
+            songName varchar(30),
+            PRIMARY KEY (userID),
+            FOREIGN KEY (userID) REFERENCES BBY_8user(ID)
+        );
 INSERT INTO BBY_8user (firstName, lastname, email, password, role, userName, age, personality)
     VALUES ("John", "Smith", "JS@test.ca", "1234", 'A', "JohnCena420", "69", "INFJ");
 INSERT INTO BBY_8user (firstName, lastname, email, password, role, userName, age, personality)
